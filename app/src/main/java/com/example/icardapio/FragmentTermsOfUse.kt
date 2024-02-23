1e9344c80f481d6be736dc1cc68db3cbd717e558
Jean Lisboa <jean@paynet.net.br>
2024-02-23 09:16:30 -0300

    Subir funcionalidade do termos de uso

---
+++ app/src/main/java/com/example/icardapio/FragmentTermsOfUse.kt
@@ -0,0 +1,27 @@
+package com.example.icardapio
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.DialogFragment
+
+class FragmentTermsOfUse : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dialog_terms_of_use, container)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val closeButton = view.findViewById<Button>(R.id.button_close)
+        closeButton.setOnClickListener {
+            dismiss()
+        }
+    }
+}
--- app/src/main/java/com/example/icardapio/MainActivity.kt
+++ app/src/main/java/com/example/icardapio/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.icardapio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.terms_of_use_button).setOnClickListener{
+            val dialog = FragmentTermsOfUse()
+            dialog.show(supportFragmentManager,dialog.tag)
+        }
     }
 }
