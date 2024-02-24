2f88673d26a10930e08c9371732fc35bf46d1073
Jean Lisboa <jean@paynet.net.br>
2024-02-24 09:16:40 -0300

    Subir tela de login

--- app/src/main/java/com/example/icardapio/MainActivity.kt
+++ app/src/main/java/com/example/icardapio/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.icardapio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -9,9 +10,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<TextView>(R.id.terms_of_use_button).setOnClickListener{
-            val dialog = FragmentTermsOfUse()
-            dialog.show(supportFragmentManager,dialog.tag)
+        findViewById<TextView>(R.id.terms_of_use_button).setOnClickListener{ //encontra o id do campo correto e faz "ouve" a ação dele
+            val dialog = FragmentTermsOfUse() //declara a variavel dialog que recebe o Fragmento que queremos chamar
+            dialog.show(supportFragmentManager,dialog.tag) //exibe o dialogo que declaramos
+        }
+
+        findViewById<TextView>(R.id.signin).setOnClickListener{ // encontra o id do textview de cadastrar
+            val intent = Intent(this,SignInOnApp :: class.java) //chama a activity da tela de cadastrar
+            startActivity(intent) // comeca a activity
+        }
+
+        findViewById<TextView>(R.id.signup).setOnClickListener{ // encontra o id do textview de entrar
+            val intenttwo = Intent(this,SignUpOnApp :: class.java) //chama a activity da tela de entrar
+            startActivity(intenttwo) // comeca a activity
         }
     }
 }
---
+++ app/src/main/java/com/example/icardapio/SignInOnApp.kt
@@ -0,0 +1,11 @@
+package com.example.icardapio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInOnApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_on_app)
+    }
+}
